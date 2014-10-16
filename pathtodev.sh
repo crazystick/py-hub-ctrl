7fb3964858b89e69fe43280c124a9dd8479741c6
Paul Adams <paul@thoughtcriminal.co.uk>
2014-10-16 14:52:08 +0000

    made hub_ctrl.py executable, added a script to get the device number from the path, e.g. 2-1.1.1

---
+++ pathtodev.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+function usage {
+	echo "Usage: `basename $0` <PATH> where path is the path as reported by udevadm, e.g. 2-1.1.1 for bus 2, hub port 1, hub port 1, hub port 1"; exit 0;
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+		-h | --help )	usage
+                            	exit
+                            	;;
+		* )		DEV_PATH=$1
+       				;;
+    esac
+    shift
+done
+
+if [ -z "$DEV_PATH" ]; then
+	usage; exit 1
+fi
+
+udevadm info /sys/bus/usb/devices/$DEV_PATH --query=property | egrep 'DEVNUM' | sed 's/DEVNUM=//g' | sed 's/^0*//'
